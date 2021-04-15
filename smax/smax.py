db81282e9db5e2318ce8c3fbea286cc5d1cf93da
TheBoringDude <iamcoderx@gmail.com>
2021-04-15 20:17:09 +0800

    add:: property - soup, function - find_all_links

--- smax/smax.py
+++ smax/smax.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+from bs4.element import ResultSet
 
 from .request import request_wrapper
 
@@ -12,6 +13,19 @@ class Smax:
     @property
     def title(self) -> str:
         """
-        Return the scrape'd website's title
+        Return the scraped website's title.
         """
         return self._soup.title.get_text()
+
+    @property
+    def soup(self) -> BeautifulSoup:
+        """
+        Return the BeautifulSoup itself.
+        """
+        return self._soup
+
+    def find_all_links(self, limit=None | int) -> ResultSet:
+        """
+        Return all links found on the document.
+        """
+        return self._soup.find_all("a", limit=limit)
